9573acea063c1881ff2aee373b6de8816c112311
SilvieFastya <sfastya@gmail.com>
2019-04-12 00:32:54 +0700

    Table Relation and Template sidebar

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,40 @@
 Route::get('/', function () {
     return view('simpeg.index');
 });
+
+//pegawai
+Route::get('addpegawai', function() {
+	return view('simpeg.addpegawai');
+})->name('addpegawai');
+
+Route::get('tabelpegawai', function() {
+	return view('simpeg.tabelpegawai');
+})->name('tabelpegawai');
+
+//keluarga
+Route::get('addkeluarga', function() {
+	return view('simpeg.addkeluarga');
+})->name('addkeluarga');
+
+Route::get('tabelkeluarga', function() {
+	return view('simpeg.tabelkeluarga');
+})->name('tabelkeluarga');
+
+//keluarga
+Route::get('addriwayat', function() {
+	return view('simpeg.addriwayat');
+})->name('addriwayat');
+
+Route::get('tabelriwayat', function() {
+	return view('simpeg.tabelriwayat');
+})->name('tabelriwayat');
+
+
+//sk
+Route::get('addsk', function() {
+	return view('simpeg.addsk');
+})->name('addsk');
+
+Route::get('tabelsk', function() {
+	return view('simpeg.tabellsk');
+})->name('tabelsk');
